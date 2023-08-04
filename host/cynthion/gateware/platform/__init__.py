09d76e423d7049735bc3368835bdd762037f54f2
Martin Ling <martin-git@earth.li>
2023-08-04 20:53:06 +0100

    Add Cynthion platform definitions.

---
+++ host/cynthion/gateware/platform/__init__.py
@@ -0,0 +1,16 @@
+#
+# This file is part of Cynthion.
+#
+# Copyright (c) 2020-2023 Great Scott Gadgets <info@greatscottgadgets.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .cynthion_r0_1 import CynthionPlatformRev0D1
+from .cynthion_r0_2 import CynthionPlatformRev0D2
+from .cynthion_r0_3 import CynthionPlatformRev0D3
+from .cynthion_r0_4 import CynthionPlatformRev0D4
+from .cynthion_r0_5 import CynthionPlatformRev0D5
+from .cynthion_r0_6 import CynthionPlatformRev0D6
+from .cynthion_r0_7 import CynthionPlatformRev0D7
+from .cynthion_r1_0 import CynthionPlatformRev1D0
+from .cynthion_r1_1 import CynthionPlatformRev1D1
+from .cynthion_r1_2 import CynthionPlatformRev1D2
