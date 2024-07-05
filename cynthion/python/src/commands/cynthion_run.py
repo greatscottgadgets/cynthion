9249f9d4d06b81888d568ce358e5a23e49a8a13f
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-07-05 16:16:13 +0200

    cli: add cynthion run command

---
+++ cynthion/python/src/commands/cynthion_run.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# This file is part of Cynthion.
+#
+# Copyright (c) 2023 Great Scott Gadgets <info@greatscottgadgets.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+""" Cynthion 'run' command. """
+
+import logging, os, subprocess, sys, tempfile
+
+from .util  import find_cynthion_asset, find_cynthion_bitstream
+from .util  import flash_soc_firmware, run_bitstream
+
+
+def cynthion_run(device, args):
+    if args.bitstream is not None:
+        run_bitstream(device, args.bitstream)
+
+    elif args.target == "analyzer":
+        run_bitstream(device, find_cynthion_bitstream(f"analyzer.bit"))
+
+    elif args.target == "facedancer":
+        flash_soc_firmware(device, find_cynthion_asset("moondancer.bin"))
+        run_bitstream(device, find_cynthion_bitstream(f"facedancer.bit"))
+
+    elif args.target == "selftest":
+        _run_selftest(device, args)
+
+    else:
+        logging.error(f"unknown target: {args.target}")
+        return
+
+
+def _run_selftest(device, args):
+    from cynthion.selftest.gateware import SelftestDevice
+    from cynthion.selftest.host import StandaloneTester
+
+    run_bitstream(device, find_cynthion_bitstream(f"selftest.bit"))
+    selftest_device = SelftestDevice()
+
+    tester = StandaloneTester(selftest_device)
+    tester.run_tests()
+    print()
