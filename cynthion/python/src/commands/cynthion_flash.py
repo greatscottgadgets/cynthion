43e364e3af09a453eb2fe6fae31320ff9729fec2
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-07-05 16:15:42 +0200

    cli: add cynthion flash command

---
+++ cynthion/python/src/commands/cynthion_flash.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# This file is part of Cynthion.
+#
+# Copyright (c) 2023 Great Scott Gadgets <info@greatscottgadgets.com>
+# SPDX-License-Identifier: BSD-3-Clause
+
+""" Cynthion 'flash' command. """
+
+import logging
+
+from .util import find_cynthion_asset, find_cynthion_bitstream
+from .util import flash_bitstream, flash_soc_firmware
+
+
+def cynthion_flash(device, args):
+    if args.bitstream is not None:
+        flash_bitstream(device, args.bitstream)
+    elif args.soc_firmware is not None:
+        flash_soc_firmware(device, args.soc_firmware)
+    elif args.target == "analyzer":
+        flash_bitstream(device, find_cynthion_bitstream("analyzer.bit"))
+    elif args.target == "facedancer":
+        flash_soc_firmware(device, find_cynthion_asset("moondancer.bin"))
+        flash_bitstream(device, find_cynthion_bitstream("facedancer.bit"))
+    elif args.target == "selftest":
+        flash_bitstream(device, find_cynthion_bitstream("selftest.bit"))
+    else:
+        logging.error(f"unknown target: {args.target}")
+        return
