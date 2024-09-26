eaa1c1a482f2d4b5a40f30515947369d90f524da
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-09-26 10:05:33 +0200

    pac: run 'make svd2rust' to update pac

--- firmware/facedancer-pac/src/generated/interrupt.rs
+++ firmware/facedancer-pac/src/generated/interrupt.rs
@@ -2,40 +2,34 @@
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 #[repr(u16)]
 pub enum Interrupt {
-    #[doc = "0 - timer"]
-    TIMER = 0,
-    #[doc = "1 - uart"]
-    UART = 1,
-    #[doc = "2 - gpioa"]
-    GPIOA = 2,
-    #[doc = "3 - gpiob"]
-    GPIOB = 3,
-    #[doc = "4 - usb0"]
-    USB0 = 4,
-    #[doc = "5 - usb0_ep_control"]
-    USB0_EP_CONTROL = 5,
-    #[doc = "6 - usb0_ep_in"]
-    USB0_EP_IN = 6,
-    #[doc = "7 - usb0_ep_out"]
-    USB0_EP_OUT = 7,
-    #[doc = "8 - usb1"]
-    USB1 = 8,
-    #[doc = "9 - usb1_ep_control"]
-    USB1_EP_CONTROL = 9,
-    #[doc = "10 - usb1_ep_in"]
-    USB1_EP_IN = 10,
-    #[doc = "11 - usb1_ep_out"]
-    USB1_EP_OUT = 11,
-    #[doc = "12 - usb2"]
-    USB2 = 12,
-    #[doc = "13 - usb2_ep_control"]
-    USB2_EP_CONTROL = 13,
-    #[doc = "14 - usb2_ep_in"]
-    USB2_EP_IN = 14,
-    #[doc = "15 - usb2_ep_out"]
-    USB2_EP_OUT = 15,
-    #[doc = "16 - uart1"]
-    UART1 = 16,
+    #[doc = "0 - timer0"]
+    TIMER0 = 0,
+    #[doc = "1 - timer1"]
+    TIMER1 = 1,
+    #[doc = "2 - usb0"]
+    USB0 = 2,
+    #[doc = "3 - usb0_ep_control"]
+    USB0_EP_CONTROL = 3,
+    #[doc = "4 - usb0_ep_in"]
+    USB0_EP_IN = 4,
+    #[doc = "5 - usb0_ep_out"]
+    USB0_EP_OUT = 5,
+    #[doc = "6 - usb1"]
+    USB1 = 6,
+    #[doc = "7 - usb1_ep_control"]
+    USB1_EP_CONTROL = 7,
+    #[doc = "8 - usb1_ep_in"]
+    USB1_EP_IN = 8,
+    #[doc = "9 - usb1_ep_out"]
+    USB1_EP_OUT = 9,
+    #[doc = "10 - usb2"]
+    USB2 = 10,
+    #[doc = "11 - usb2_ep_control"]
+    USB2_EP_CONTROL = 11,
+    #[doc = "12 - usb2_ep_in"]
+    USB2_EP_IN = 12,
+    #[doc = "13 - usb2_ep_out"]
+    USB2_EP_OUT = 13,
 }
 #[doc = r" TryFromInterruptError"]
 #[derive(Debug, Copy, Clone)]
@@ -45,23 +39,20 @@ impl Interrupt {
     #[inline]
     pub fn try_from(value: u8) -> Result<Self, TryFromInterruptError> {
         match value {
-            0 => Ok(Interrupt::TIMER),
-            1 => Ok(Interrupt::UART),
-            2 => Ok(Interrupt::GPIOA),
-            3 => Ok(Interrupt::GPIOB),
-            4 => Ok(Interrupt::USB0),
-            5 => Ok(Interrupt::USB0_EP_CONTROL),
-            6 => Ok(Interrupt::USB0_EP_IN),
-            7 => Ok(Interrupt::USB0_EP_OUT),
-            8 => Ok(Interrupt::USB1),
-            9 => Ok(Interrupt::USB1_EP_CONTROL),
-            10 => Ok(Interrupt::USB1_EP_IN),
-            11 => Ok(Interrupt::USB1_EP_OUT),
-            12 => Ok(Interrupt::USB2),
-            13 => Ok(Interrupt::USB2_EP_CONTROL),
-            14 => Ok(Interrupt::USB2_EP_IN),
-            15 => Ok(Interrupt::USB2_EP_OUT),
-            16 => Ok(Interrupt::UART1),
+            0 => Ok(Interrupt::TIMER0),
+            1 => Ok(Interrupt::TIMER1),
+            2 => Ok(Interrupt::USB0),
+            3 => Ok(Interrupt::USB0_EP_CONTROL),
+            4 => Ok(Interrupt::USB0_EP_IN),
+            5 => Ok(Interrupt::USB0_EP_OUT),
+            6 => Ok(Interrupt::USB1),
+            7 => Ok(Interrupt::USB1_EP_CONTROL),
+            8 => Ok(Interrupt::USB1_EP_IN),
+            9 => Ok(Interrupt::USB1_EP_OUT),
+            10 => Ok(Interrupt::USB2),
+            11 => Ok(Interrupt::USB2_EP_CONTROL),
+            12 => Ok(Interrupt::USB2_EP_IN),
+            13 => Ok(Interrupt::USB2_EP_OUT),
             _ => Err(TryFromInterruptError(())),
         }
     }
--- firmware/facedancer-pac/src/generated/usb1_ep_in/data.rs
+++ firmware/facedancer-pac/src/generated/usb1_ep_in/data.rs
@@ -1,27 +1,31 @@
+#[doc = "Register `data` reader"]
+pub type R = crate::R<DATA_SPEC>;
 #[doc = "Register `data` writer"]
 pub type W = crate::W<DATA_SPEC>;
-#[doc = "Field `data` writer - Write-only register. Each write enqueues a byte to be transmitted; gradually building a single packet to be transmitted. This queue should only ever contain a single packet; it is the software's responsibility to handle breaking requests down into packets."]
-pub type DATA_W<'a, REG> = crate::FieldWriter<'a, REG, 8>;
+#[doc = "Field `byte` writer - TODO amaranth_soc/csr/reg.py:471"]
+pub type BYTE_W<'a, REG> = crate::FieldWriter<'a, REG, 8>;
 impl W {
-    #[doc = "Bits 0:7 - Write-only register. Each write enqueues a byte to be transmitted; gradually building a single packet to be transmitted. This queue should only ever contain a single packet; it is the software's responsibility to handle breaking requests down into packets."]
+    #[doc = "Bits 0:7 - TODO amaranth_soc/csr/reg.py:471"]
     #[inline(always)]
     #[must_use]
-    pub fn data(&mut self) -> DATA_W<DATA_SPEC> {
-        DATA_W::new(self, 0)
+    pub fn byte(&mut self) -> BYTE_W<DATA_SPEC> {
+        BYTE_W::new(self, 0)
     }
 }
-#[doc = "usb1_ep_in data register\n\nYou can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`data::W`](W). See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+#[doc = "TODO amaranth_soc/csr/reg.py:471\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`data::R`](R).  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`data::W`](W). You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
 pub struct DATA_SPEC;
 impl crate::RegisterSpec for DATA_SPEC {
-    type Ux = u32;
+    type Ux = u8;
 }
+#[doc = "`read()` method returns [`data::R`](R) reader structure"]
+impl crate::Readable for DATA_SPEC {}
 #[doc = "`write(|w| ..)` method takes [`data::W`](W) writer structure"]
 impl crate::Writable for DATA_SPEC {
     type Safety = crate::Unsafe;
-    const ZERO_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
-    const ONE_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
+    const ZERO_TO_MODIFY_FIELDS_BITMAP: u8 = 0;
+    const ONE_TO_MODIFY_FIELDS_BITMAP: u8 = 0;
 }
 #[doc = "`reset()` method sets data to value 0"]
 impl crate::Resettable for DATA_SPEC {
-    const RESET_VALUE: u32 = 0;
+    const RESET_VALUE: u8 = 0;
 }
