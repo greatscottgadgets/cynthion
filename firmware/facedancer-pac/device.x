22e83fec9f841fb1762eecd32ff7b977c34953ac
Antoine van Gelder <antoine@greatscottgadgets.com>
2024-09-26 10:05:33 +0200

    pac: run 'make svd2rust' to update pac

--- firmware/facedancer-pac/device.x
+++ firmware/facedancer-pac/device.x
@@ -1,7 +1,5 @@
-PROVIDE(TIMER = DefaultHandler);
-PROVIDE(UART = DefaultHandler);
-PROVIDE(GPIOA = DefaultHandler);
-PROVIDE(GPIOB = DefaultHandler);
+PROVIDE(TIMER0 = DefaultHandler);
+PROVIDE(TIMER1 = DefaultHandler);
 PROVIDE(USB0 = DefaultHandler);
 PROVIDE(USB0_EP_CONTROL = DefaultHandler);
 PROVIDE(USB0_EP_IN = DefaultHandler);
@@ -14,5 +12,4 @@ PROVIDE(USB2 = DefaultHandler);
 PROVIDE(USB2_EP_CONTROL = DefaultHandler);
 PROVIDE(USB2_EP_IN = DefaultHandler);
 PROVIDE(USB2_EP_OUT = DefaultHandler);
-PROVIDE(UART1 = DefaultHandler);
 
--- firmware/facedancer-pac/src/generated/spi0.rs
+++ firmware/facedancer-pac/src/generated/spi0.rs
@@ -1,90 +1,55 @@
 #[repr(C)]
 #[doc = "Register block"]
 pub struct RegisterBlock {
-    phy_len: PHY_LEN,
-    phy_width: PHY_WIDTH,
-    phy_mask: PHY_MASK,
+    phy: PHY,
     cs: CS,
-    rxtx: RXTX,
-    tx_rdy: TX_RDY,
-    rx_rdy: RX_RDY,
+    _reserved2: [u8; 0x03],
+    rx: RX,
+    tx: TX,
 }
 impl RegisterBlock {
-    #[doc = "0x00 - spi0 phy_len register"]
+    #[doc = "0x00 - TODO amaranth_soc/csr/reg.py:471"]
     #[inline(always)]
-    pub const fn phy_len(&self) -> &PHY_LEN {
-        &self.phy_len
+    pub const fn phy(&self) -> &PHY {
+        &self.phy
     }
-    #[doc = "0x01 - spi0 phy_width register"]
-    #[inline(always)]
-    pub const fn phy_width(&self) -> &PHY_WIDTH {
-        &self.phy_width
-    }
-    #[doc = "0x02 - spi0 phy_mask register"]
-    #[inline(always)]
-    pub const fn phy_mask(&self) -> &PHY_MASK {
-        &self.phy_mask
-    }
-    #[doc = "0x03 - spi0 cs register"]
+    #[doc = "0x04 - TODO amaranth_soc/csr/reg.py:471"]
     #[inline(always)]
     pub const fn cs(&self) -> &CS {
         &self.cs
     }
-    #[doc = "0x04 - spi0 rxtx register"]
+    #[doc = "0x08..0x10 - TODO amaranth_soc/csr/reg.py:471"]
     #[inline(always)]
-    pub const fn rxtx(&self) -> &RXTX {
-        &self.rxtx
+    pub const fn rx(&self) -> &RX {
+        &self.rx
     }
-    #[doc = "0x08 - spi0 tx_rdy register"]
+    #[doc = "0x10..0x18 - TODO amaranth_soc/csr/reg.py:471"]
     #[inline(always)]
-    pub const fn tx_rdy(&self) -> &TX_RDY {
-        &self.tx_rdy
-    }
-    #[doc = "0x09 - spi0 rx_rdy register"]
-    #[inline(always)]
-    pub const fn rx_rdy(&self) -> &RX_RDY {
-        &self.rx_rdy
+    pub const fn tx(&self) -> &TX {
+        &self.tx
     }
 }
-#[doc = "phy_len (rw) register accessor: spi0 phy_len register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`phy_len::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`phy_len::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@phy_len`]
+#[doc = "phy (rw) register accessor: TODO amaranth_soc/csr/reg.py:471\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`phy::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`phy::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@phy`]
 module"]
-#[doc(alias = "phy_len")]
-pub type PHY_LEN = crate::Reg<phy_len::PHY_LEN_SPEC>;
-#[doc = "spi0 phy_len register"]
-pub mod phy_len;
-#[doc = "phy_width (rw) register accessor: spi0 phy_width register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`phy_width::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`phy_width::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@phy_width`]
-module"]
-#[doc(alias = "phy_width")]
-pub type PHY_WIDTH = crate::Reg<phy_width::PHY_WIDTH_SPEC>;
-#[doc = "spi0 phy_width register"]
-pub mod phy_width;
-#[doc = "phy_mask (rw) register accessor: spi0 phy_mask register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`phy_mask::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`phy_mask::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@phy_mask`]
-module"]
-#[doc(alias = "phy_mask")]
-pub type PHY_MASK = crate::Reg<phy_mask::PHY_MASK_SPEC>;
-#[doc = "spi0 phy_mask register"]
-pub mod phy_mask;
-#[doc = "cs (rw) register accessor: spi0 cs register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`cs::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`cs::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@cs`]
+#[doc(alias = "phy")]
+pub type PHY = crate::Reg<phy::PHY_SPEC>;
+#[doc = "TODO amaranth_soc/csr/reg.py:471"]
+pub mod phy;
+#[doc = "cs (rw) register accessor: TODO amaranth_soc/csr/reg.py:471\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`cs::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`cs::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@cs`]
 module"]
 #[doc(alias = "cs")]
 pub type CS = crate::Reg<cs::CS_SPEC>;
-#[doc = "spi0 cs register"]
+#[doc = "TODO amaranth_soc/csr/reg.py:471"]
 pub mod cs;
-#[doc = "rxtx (rw) register accessor: spi0 rxtx register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`rxtx::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`rxtx::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@rxtx`]
+#[doc = "rx (rw) register accessor: TODO amaranth_soc/csr/reg.py:471\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`rx::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`rx::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@rx`]
 module"]
-#[doc(alias = "rxtx")]
-pub type RXTX = crate::Reg<rxtx::RXTX_SPEC>;
-#[doc = "spi0 rxtx register"]
-pub mod rxtx;
-#[doc = "tx_rdy (r) register accessor: spi0 tx_rdy register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`tx_rdy::R`].  See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@tx_rdy`]
+#[doc(alias = "rx")]
+pub type RX = crate::Reg<rx::RX_SPEC>;
+#[doc = "TODO amaranth_soc/csr/reg.py:471"]
+pub mod rx;
+#[doc = "tx (rw) register accessor: TODO amaranth_soc/csr/reg.py:471\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`tx::R`].  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`tx::W`]. You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@tx`]
 module"]
-#[doc(alias = "tx_rdy")]
-pub type TX_RDY = crate::Reg<tx_rdy::TX_RDY_SPEC>;
-#[doc = "spi0 tx_rdy register"]
-pub mod tx_rdy;
-#[doc = "rx_rdy (r) register accessor: spi0 rx_rdy register\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`rx_rdy::R`].  See [API](https://docs.rs/svd2rust/#read--modify--write-api).\n\nFor information about available fields see [`mod@rx_rdy`]
-module"]
-#[doc(alias = "rx_rdy")]
-pub type RX_RDY = crate::Reg<rx_rdy::RX_RDY_SPEC>;
-#[doc = "spi0 rx_rdy register"]
-pub mod rx_rdy;
+#[doc(alias = "tx")]
+pub type TX = crate::Reg<tx::TX_SPEC>;
+#[doc = "TODO amaranth_soc/csr/reg.py:471"]
+pub mod tx;
