d209e9d6913252235bcb13c403d92d62fb64e02b
Antoine van Gelder <antoine@greatscottgadgets.com>
2025-01-23 14:34:29 +0200

    pac: update svd & run svd2rust

--- firmware/moondancer-pac/src/generated/timer1/reload.rs
+++ firmware/moondancer-pac/src/generated/timer1/reload.rs
@@ -21,7 +21,7 @@ impl W {
         VALUE_W::new(self, 0)
     }
 }
-#[doc = "Reload value of counter. When counter reaches 0 is is automatically reloaded with this value.\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`reload::R`](R).  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`reload::W`](W). You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+#[doc = "Reload value of counter.\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`reload::R`](R).  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`reload::W`](W). You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
 pub struct RELOAD_SPEC;
 impl crate::RegisterSpec for RELOAD_SPEC {
     type Ux = u32;
