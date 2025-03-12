0f7a8a77d9002df3be866c6ea4a363dbd72a42f2
Antoine van Gelder <antoine@greatscottgadgets.com>
2025-03-12 15:29:37 +0200

    pac: run svd2rust

--- firmware/moondancer-pac/src/generated/gpio0/mode.rs
+++ firmware/moondancer-pac/src/generated/gpio0/mode.rs
@@ -79,55 +79,46 @@ impl R {
 impl W {
     #[doc = "Bits 0:1 - pin_0 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_0(&mut self) -> PIN_0_W<MODE_SPEC> {
         PIN_0_W::new(self, 0)
     }
     #[doc = "Bits 2:3 - pin_1 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_1(&mut self) -> PIN_1_W<MODE_SPEC> {
         PIN_1_W::new(self, 2)
     }
     #[doc = "Bits 4:5 - pin_2 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_2(&mut self) -> PIN_2_W<MODE_SPEC> {
         PIN_2_W::new(self, 4)
     }
     #[doc = "Bits 6:7 - pin_3 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_3(&mut self) -> PIN_3_W<MODE_SPEC> {
         PIN_3_W::new(self, 6)
     }
     #[doc = "Bits 8:9 - pin_4 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_4(&mut self) -> PIN_4_W<MODE_SPEC> {
         PIN_4_W::new(self, 8)
     }
     #[doc = "Bits 10:11 - pin_5 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_5(&mut self) -> PIN_5_W<MODE_SPEC> {
         PIN_5_W::new(self, 10)
     }
     #[doc = "Bits 12:13 - pin_6 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_6(&mut self) -> PIN_6_W<MODE_SPEC> {
         PIN_6_W::new(self, 12)
     }
     #[doc = "Bits 14:15 - pin_7 field"]
     #[inline(always)]
-    #[must_use]
     pub fn pin_7(&mut self) -> PIN_7_W<MODE_SPEC> {
         PIN_7_W::new(self, 14)
     }
 }
-#[doc = "Mode register. This :class:`csr.Register` contains an array of ``pin_count`` read/write fields. Each field is 2-bit wide and its possible values are defined by the :class:`PinMode` enumeration. If ``pin_count`` is 8, then the register has the following fields: .. bitfield:: :bits: 16 \\[ { \"name\": \"pin\\[0\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[1\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[2\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[3\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[4\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[5\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[6\\]\", \"bits\": 2, \"attr\": \"RW\" }, { \"name\": \"pin\\[7\\]\", \"bits\": 2, \"attr\": \"RW\" }, \\]
-Parameters ---------- pin_count : :class:`int` Number of GPIO pins.\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`mode::R`](R).  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`mode::W`](W). You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+#[doc = "A CSR register. Parameters ---------- fields : :class:`dict` or :class:`list` or :class:`Field` Collection of register fields. If ``None`` (default), a dict is populated from Python :term:`variable annotations <python:variable annotations>`. ``fields`` is used to create a :class:`FieldActionMap`, :class:`FieldActionArray`, or :class:`FieldAction`, depending on its type (dict, list, or Field). Interface attributes -------------------- element : :class:`Element` Interface between this register and a CSR bus primitive. Attributes ---------- field : :class:`FieldActionMap` or :class:`FieldActionArray` or :class:`FieldAction` Collection of field instances. f : :class:`FieldActionMap` or :class:`FieldActionArray` or :class:`FieldAction` Shorthand for :attr:`Register.field`. Raises ------ :exc:`TypeError` If ``fields`` is neither ``None``, a :class:`dict`, a :class:`list`, or a :class:`Field`. :exc:`ValueError` If ``fields`` is not ``None`` and at least one variable annotation is a :class:`Field`. :exc:`ValueError` If ``element.access`` is not readable and at least one field is readable. :exc:`ValueError` If ``element.access`` is not writable and at least one field is writable.\n\nYou can [`read`](crate::Reg::read) this register and get [`mode::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`mode::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
 pub struct MODE_SPEC;
 impl crate::RegisterSpec for MODE_SPEC {
     type Ux = u16;
--- firmware/moondancer-pac/src/generated/usb0_ep_out/reset.rs
+++ firmware/moondancer-pac/src/generated/usb0_ep_out/reset.rs
@@ -18,18 +18,16 @@ impl R {
 impl W {
     #[doc = "Bit 0 - fifo field"]
     #[inline(always)]
-    #[must_use]
     pub fn fifo(&mut self) -> FIFO_W<RESET_SPEC> {
         FIFO_W::new(self, 0)
     }
     #[doc = "Bits 1:7 - _0 field"]
     #[inline(always)]
-    #[must_use]
     pub fn _0(&mut self) -> _0_W<RESET_SPEC> {
         _0_W::new(self, 1)
     }
 }
-#[doc = "Reset register fifo: Local reset for the OUT handler; clears the out FIFO.\n\nYou can [`read`](crate::generic::Reg::read) this register and get [`reset::R`](R).  You can [`reset`](crate::generic::Reg::reset), [`write`](crate::generic::Reg::write), [`write_with_zero`](crate::generic::Reg::write_with_zero) this register using [`reset::W`](W). You can also [`modify`](crate::generic::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+#[doc = "A CSR register. Parameters ---------- fields : :class:`dict` or :class:`list` or :class:`Field` Collection of register fields. If ``None`` (default), a dict is populated from Python :term:`variable annotations <python:variable annotations>`. ``fields`` is used to create a :class:`FieldActionMap`, :class:`FieldActionArray`, or :class:`FieldAction`, depending on its type (dict, list, or Field). Interface attributes -------------------- element : :class:`Element` Interface between this register and a CSR bus primitive. Attributes ---------- field : :class:`FieldActionMap` or :class:`FieldActionArray` or :class:`FieldAction` Collection of field instances. f : :class:`FieldActionMap` or :class:`FieldActionArray` or :class:`FieldAction` Shorthand for :attr:`Register.field`. Raises ------ :exc:`TypeError` If ``fields`` is neither ``None``, a :class:`dict`, a :class:`list`, or a :class:`Field`. :exc:`ValueError` If ``fields`` is not ``None`` and at least one variable annotation is a :class:`Field`. :exc:`ValueError` If ``element.access`` is not readable and at least one field is readable. :exc:`ValueError` If ``element.access`` is not writable and at least one field is writable.\n\nYou can [`read`](crate::Reg::read) this register and get [`reset::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reset::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
 pub struct RESET_SPEC;
 impl crate::RegisterSpec for RESET_SPEC {
     type Ux = u8;
