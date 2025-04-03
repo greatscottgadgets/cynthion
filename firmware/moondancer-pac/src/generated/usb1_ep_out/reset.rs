95df9eba4ea5e4275c46caa577aaf135cc9f6f16
Antoine van Gelder <antoine@greatscottgadgets.com>
2025-04-03 16:43:25 +0200

    pac: run svd2rust

--- firmware/moondancer-pac/src/generated/usb1_ep_out/reset.rs
+++ firmware/moondancer-pac/src/generated/usb1_ep_out/reset.rs
@@ -2,15 +2,9 @@
 pub type R = crate::R<RESET_SPEC>;
 #[doc = "Register `reset` writer"]
 pub type W = crate::W<RESET_SPEC>;
-<<<<<<<< HEAD:firmware/moondancer-pac/src/generated/usb1_ep_out/reset.rs
 #[doc = "Field `fifo` writer - fifo field"]
 pub type FIFO_W<'a, REG> = crate::BitWriter<'a, REG>;
 #[doc = "Field `_0` reader - _0 field"]
-========
-#[doc = "Field `fifo` writer - TODO amaranth_soc/csr/reg.py:471"]
-pub type FIFO_W<'a, REG> = crate::BitWriter<'a, REG>;
-#[doc = "Field `_0` reader - TODO amaranth_soc/csr/reg.py:471"]
->>>>>>>> 0fd62aa (repo: post-rebase fixes):firmware/moondancer-pac/src/generated/usb1_ep_control/reset.rs
 pub type _0_R = crate::FieldReader;
 #[doc = "Field `_0` writer - _0 field"]
 pub type _0_W<'a, REG> = crate::FieldWriter<'a, REG, 7>;
@@ -33,7 +27,7 @@ impl W {
         _0_W::new(self, 1)
     }
 }
-#[doc = "A CSR register. Parameters ---------- fields : :class:`dict` or :class:`list` or :class:`Field` Collection of register fields. If ``None`` (default), a dict is populated from Python :term:`variable annotations <python:variable annotations>`. ``fields`` is used to create a :class:`FieldActionMap`, :class:`FieldActionArray`, or :class:`FieldAction`, depending on its type (dict, list, or Field). Interface attributes -------------------- element : :class:`Element` Interface between this register and a CSR bus primitive. Attributes ---------- field : :class:`FieldActionMap` or :class:`FieldActionArray` or :class:`FieldAction` Collection of field instances. f : :class:`FieldActionMap` or :class:`FieldActionArray` or :class:`FieldAction` Shorthand for :attr:`Register.field`. Raises ------ :exc:`TypeError` If ``fields`` is neither ``None``, a :class:`dict`, a :class:`list`, or a :class:`Field`. :exc:`ValueError` If ``fields`` is not ``None`` and at least one variable annotation is a :class:`Field`. :exc:`ValueError` If ``element.access`` is not readable and at least one field is readable. :exc:`ValueError` If ``element.access`` is not writable and at least one field is writable.\n\nYou can [`read`](crate::Reg::read) this register and get [`reset::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reset::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
+#[doc = "Reset register fifo: Local reset for the OUT handler; clears the out FIFO.\n\nYou can [`read`](crate::Reg::read) this register and get [`reset::R`](R). You can [`reset`](crate::Reg::reset), [`write`](crate::Reg::write), [`write_with_zero`](crate::Reg::write_with_zero) this register using [`reset::W`](W). You can also [`modify`](crate::Reg::modify) this register. See [API](https://docs.rs/svd2rust/#read--modify--write-api)."]
 pub struct RESET_SPEC;
 impl crate::RegisterSpec for RESET_SPEC {
     type Ux = u8;
